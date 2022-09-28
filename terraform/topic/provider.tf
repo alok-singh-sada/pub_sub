7a635e2226cc219bb8369a9158ec0bf52d4f9b96
Alok Singh <alok.singh@sada.com>
2022-09-28 21:11:47 +0530

    automate

--- terraform/topic/provider.tf
+++ terraform/topic/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.13.0"
-    }
-  }
-}
-
 provider "google" {
   credentials = file(TF_VAR_SA)
 }
